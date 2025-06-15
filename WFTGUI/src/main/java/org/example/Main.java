9367be435d11d87375cb1e7365c28d9e35562a22
Einav <einavco786@gmail.com>
2025-06-15 22:50:45 +0300

    feat(Main.java, GUI.java): Connect and Disconnect via inputs

--- WFTGUI/src/main/java/org/example/Main.java
+++ WFTGUI/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
-    }
+public class Main {
+  private static void createAndShowGUI() {
+    // Create and set up the window.
+    JFrame frame = new JFrame("WFT");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    // Add the ubiquitous "Hello World" label.
+    JLabel label = new JLabel("Hello World");
+    frame.getContentPane().add(label);
+
+    // Display the window.
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    // Schedule a job for the event-dispatching thread:
+    // creating and showing this application's GUI.
+    javax.swing.SwingUtilities.invokeLater(
+        new Runnable() {
+          public void run() {
+            createAndShowGUI();
+          }
+        });
+  }
 }
