6ea61d05c17fc8517fd08c89e198f2255b34efe8
Einav <einavco786@gmail.com>
2025-06-15 23:02:02 +0300

    feat(Main.java, GUI.java): implement swing GUI and able to connect and disconnect to client

--- WFTGUI/src/main/java/org/example/Main.java
+++ WFTGUI/src/main/java/org/example/Main.java
@@ -1,30 +1,48 @@
 package org.example;
 
-import javax.swing.*;
+import java.io.IOException;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.xfer.FileSystemFile;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-  private static void createAndShowGUI() {
-    // Create and set up the window.
-    JFrame frame = new JFrame("WFT");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    final static String hostIP = "192.168.1.194";
+    final static String username = "einav";
+    final static String password = "2121";
+    final static String filePath = "C:/Users/user/Desktop/Yuval.txt";
 
-    // Add the ubiquitous "Hello World" label.
-    JLabel label = new JLabel("Hello World");
-    frame.getContentPane().add(label);
+    public static void main(String[] args) {
+        try {
+            SSHClient client = new SSHClient();
+            client.addHostKeyVerifier(new PromiscuousVerifier());
+            client.connect(hostIP);
+            client.authPassword(username, password);
+            SFTPClient sftp = client.newSFTPClient();
+            sftp.put(new FileSystemFile(filePath), "/Users/einav/Desktop");
+            sftp.close();
+            client.disconnect();
+            System.out.println("File transferred successfully");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 
-    // Display the window.
-    frame.pack();
-    frame.setVisible(true);
-  }
+    public static SSHClient connect(String hostIP, String username, String password) throws IOException {
+        SSHClient client = new SSHClient();
+        client.addHostKeyVerifier(new PromiscuousVerifier());
+        client.connect(hostIP);
+        client.authPassword(username, password);
+        return client;
+    }
 
-  public static void main(String[] args) {
-    // Schedule a job for the event-dispatching thread:
-    // creating and showing this application's GUI.
-    javax.swing.SwingUtilities.invokeLater(
-        new Runnable() {
-          public void run() {
-            createAndShowGUI();
-          }
-        });
-  }
+    public static void disconnect(SSHClient client) {
+        try {
+            client.disconnect();
+        } catch (Exception e) {
+            System.out.println("Disconnection failed");
+        }
+    }
 }
